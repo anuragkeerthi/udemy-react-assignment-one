4f306eafdb237444128af62f0aee518d9007cdf8
Anurag Keerthi <anuragkeerthi@gmail.com>
2020-09-21 20:40:51 +0530

    Initial Commit

---
+++ src/UserOutput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class UserOutput extends Component {
+    render() {
+        return (
+            <div>
+                <p>My name is </p>
+                <p>I am   years old.</p>
+            </div>
+        )
+    }
+}
+
+export default UserOutput
